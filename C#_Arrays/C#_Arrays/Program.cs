8fa6c32ae31bc4d240443c98023e22c6a830106a
Haseeb1389446 <haseeb1389446@gmail.com>
2024-12-13 04:45:12 -0800

    Console application || practices

---
+++ C#_Arrays/C#_Arrays/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //------------ Example 1 -------------
+
+            //string[] myarr = new string[3];
+
+            //myarr[0] = "John";
+            //myarr[1] = "Freddy";
+            //myarr[2] = "Mic";
+
+            //Console.WriteLine(myarr[1]);
+            //Console.ReadKey();
+
+            //------------ Example 2 -------------
+
+            //string[] myarr = new string[] { "John", "Freddy", "Mic" };
+
+            //Console.WriteLine(myarr[0]);
+            //Console.ReadKey();
+
+            //------------ Example 3 -------------
+
+            //string[] myarr = { "John", "Freddy", "Mic" };
+
+            //Console.WriteLine(myarr[3]);
+            //Console.ReadKey();
+
+            //------------ Array With For Loop ---------
+
+            //int[] myarr = { 20, 50, 75, 32, 14 };
+
+            //for (int i = 0; i < myarr.Length; i++)
+            //{
+            //    Console.WriteLine(myarr[i]);
+            //}
+
+            //Console.ReadKey();
+
+            //----------- Array With ForEach Loop -------
+
+            //int[] myarr = { 54, 23, 51, 54, 89 };
+
+            //foreach (int index in myarr)
+            //{
+            //    Console.WriteLine(index);
+            //}
+
+            //Console.ReadKey();
+
+            //----------- Double Dimension Array ------------
+
+            //int[,] myarr = new int[3, 4]
+            //{
+            //    { 45, 20, 75, 50 },
+            //    { 20, 54, 89, 28 },
+            //    { 46, 76, 94, 23 }
+            //};
+
+            //for ( int i = 0; i < myarr.GetLength(0); i++)
+            //{
+            //    for ( int j = 0; j < myarr.GetLength(1); j++ )
+            //    {
+            //        Console.WriteLine(myarr[i, j] + " ");    // Verticle  
+            //        Console.Write(myarr[i, j] + " ");          // Horizontal
+            //    }
+
+            //    Console.WriteLine();
+
+            //}
+
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
