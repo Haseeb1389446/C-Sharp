3c663ef1ea58efc27f4111330d874119d1400bc8
Haseeb1389446 <haseeb1389446@gmail.com>
2024-12-14 00:56:28 -0800

    Console application || practices

--- C#_Arrays/C#_Arrays/Program.cs
+++ C#_Arrays/C#_Arrays/Program.cs
@@ -13,75 +13,120 @@ namespace C__Arrays
 
             //------------ Example 1 -------------
 
-            //string[] myarr = new string[3];
+            string[] myarr = new string[3];
 
-            //myarr[0] = "John";
-            //myarr[1] = "Freddy";
-            //myarr[2] = "Mic";
+            myarr[0] = "John";
+            myarr[1] = "Freddy";
+            myarr[2] = "Mic";
 
-            //Console.WriteLine(myarr[1]);
-            //Console.ReadKey();
+            Console.WriteLine(myarr[1]);
+            Console.ReadKey();
 
             //------------ Example 2 -------------
 
-            //string[] myarr = new string[] { "John", "Freddy", "Mic" };
+            string[] myarr = new string[] { "John", "Freddy", "Mic" };
 
-            //Console.WriteLine(myarr[0]);
-            //Console.ReadKey();
+            Console.WriteLine(myarr[0]);
+            Console.ReadKey();
 
             //------------ Example 3 -------------
 
-            //string[] myarr = { "John", "Freddy", "Mic" };
+            string[] myarr = { "John", "Freddy", "Mic" };
 
-            //Console.WriteLine(myarr[3]);
-            //Console.ReadKey();
+            Console.WriteLine(myarr[3]);
+            Console.ReadKey();
 
             //------------ Array With For Loop ---------
 
-            //int[] myarr = { 20, 50, 75, 32, 14 };
+            int[] myarr = { 20, 50, 75, 32, 14 };
 
-            //for (int i = 0; i < myarr.Length; i++)
-            //{
-            //    Console.WriteLine(myarr[i]);
-            //}
+            for (int i = 0; i < myarr.Length; i++)
+            {
+                Console.WriteLine(myarr[i]);
+            }
 
-            //Console.ReadKey();
+            Console.ReadKey();
 
             //----------- Array With ForEach Loop -------
 
-            //int[] myarr = { 54, 23, 51, 54, 89 };
+            int[] myarr = { 54, 23, 51, 54, 89 };
 
-            //foreach (int index in myarr)
-            //{
-            //    Console.WriteLine(index);
-            //}
+            foreach (int index in myarr)
+            {
+                Console.WriteLine(index);
+            }
 
-            //Console.ReadKey();
+            Console.ReadKey();
 
             //----------- Double Dimension Array ------------
 
-            //int[,] myarr = new int[3, 4]
-            //{
-            //    { 45, 20, 75, 50 },
-            //    { 20, 54, 89, 28 },
-            //    { 46, 76, 94, 23 }
-            //};
+            int[,] myarr = new int[3, 4]
+            {
+                { 45, 20, 75, 50 },
+                { 20, 54, 89, 28 },
+                { 46, 76, 94, 23 }
+            };
 
-            //for ( int i = 0; i < myarr.GetLength(0); i++)
-            //{
-            //    for ( int j = 0; j < myarr.GetLength(1); j++ )
-            //    {
-            //        Console.WriteLine(myarr[i, j] + " ");    // Verticle  
-            //        Console.Write(myarr[i, j] + " ");          // Horizontal
-            //    }
+            for (int i = 0; i < myarr.GetLength(0); i++)
+            {
+                for (int j = 0; j < myarr.GetLength(1); j++)
+                {
+                    Console.WriteLine(myarr[i, j] + " ");    // Verticle  
+                    Console.Write(myarr[i, j] + " ");          // Horizontal
+                }
 
-            //    Console.WriteLine();
+                Console.WriteLine();
 
-            //}
+            }
 
-            //Console.ReadKey();
+            Console.ReadKey();
+
+            //----------- Jagged Array -----------------
+
+            int[][] myarr = new int[3][];
+
+            myarr[0] = new[] { 50, 12, 30, 56, 85 };
+            myarr[1] = new[] { 12, 45, 23 };
+            myarr[2] = new[] { 91, 23, 58, 94, 74, 63 };
 
 
+            for (int i = 0; i < myarr.GetLength(0); i++)
+            {
+                for (int j = 0; j < myarr[i].Length; j++)
+                {
+                    Console.Write(myarr[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+            //------------- Array With User Input -----------
+
+            Console.WriteLine("Enter The Array Length:");
+            int arrlength = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            int[] myarr = new int[arrlength];
+
+            for ( int i = 0; i < arrlength; i++ )
+            {
+                Console.WriteLine($"Enter The {i} Array Value:");
+                int arrvalue = int.Parse(Console.ReadLine());
+                myarr[i] = arrvalue;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("----------- Array Data ---------");
+
+            foreach (int index in myarr)
+            {
+                Console.WriteLine(index);
+            }
+
+            Console.ReadKey();
 
         }
     }
