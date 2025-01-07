058826812529bcbeb09ff137ec5040062b3592b8
Haseeb1389446 <haseeb1389446@gmail.com>
2025-01-06 22:20:13 -0800

    Console application || practices

---
+++ Delegates_And _Generics/Delegates_And _Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_And__Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Addition
+
+            calculation add = new calculation(operations.addition);
+            Console.WriteLine(add(12, 3));
+
+            // Subtraction
+
+            calculation sub = new calculation(operations.subtraction);
+            Console.WriteLine(sub(20, 2));
+
+            // Multiplication
+
+            calculation mul = new calculation(operations.multiplication);
+            Console.WriteLine(mul(10, 12));
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Delegates_And _Generics/Delegates_And _Generics/operations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_And__Generics
+{
+
+    public delegate int calculation(int num1, int num2);
+
+    internal class operations
+    {
+
+        public static int addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+    }
+}
