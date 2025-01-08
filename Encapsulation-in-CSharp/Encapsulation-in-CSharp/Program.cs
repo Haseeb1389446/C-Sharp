afa7061d4840cd4b73450df3c3ccd2af581a7b92
Haseeb1389446 <haseeb1389446@gmail.com>
2025-01-08 00:40:01 -0800

    Console application || practices

---
+++ Encapsulation-in-CSharp/Encapsulation-in-CSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_in_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Encapsulation
+
+            employee emp = new employee();
+
+            emp.setData(101, "Ali", "Manager", 100000);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Encapsulation-in-CSharp/Encapsulation-in-CSharp/employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_in_CSharp
+{
+    internal class employee
+    {
+
+        private int empID;
+        private string empName;
+        private string empDesignation;
+        private int empSalary;
+
+        public void setData(int id, string name, string designation, int salary)
+        {
+
+            empID = id;
+            empName = name;
+            empDesignation = designation;
+            empSalary = salary;
+
+            Console.WriteLine(this.empID);
+            Console.WriteLine(this.empName);
+            Console.WriteLine(this.empDesignation);
+            Console.WriteLine(this.empSalary);
+
+        }
+
+    }
+}
