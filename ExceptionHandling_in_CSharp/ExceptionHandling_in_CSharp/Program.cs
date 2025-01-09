0be92024d61bebf877639b380f6a164200c35bdc
Haseeb1389446 <haseeb1389446@gmail.com>
2025-01-08 22:09:10 -0800

    Console application || practices

--- ExceptionHandling_in_CSharp/ExceptionHandling_in_CSharp/Program.cs
+++ ExceptionHandling_in_CSharp/ExceptionHandling_in_CSharp/Program.cs
@@ -10,6 +10,73 @@ namespace ExceptionHandling_in_CSharp
     {
         static void Main(string[] args)
         {
+
+            //Example 1
+
+            Console.WriteLine("****** Division *****");
+            Console.WriteLine();
+
+            // try
+            // {
+            // Console.WriteLine("Enter NUM1 : ");
+            // int num1 = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Enter NUM2 : ");
+            // int num2 = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("Division rezult is : " + num1 / num2);
+            // }
+            // catch(Exception ex)
+            // {
+            //    Console.WriteLine($"Error: {ex.Message}");
+            // }
+
+            try
+            {
+                Console.WriteLine("Enter NUM1 : ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter NUM2 : ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Division rezult is : " + num1 / num2);
+            }
+            catch (DivideByZeroException ex1)
+            {
+                Console.WriteLine($"Error: {ex1.Message}");
+            }
+            catch (FormatException ex2)
+            {
+                Console.WriteLine($"Error: {ex2.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Execution Completed...");
+            }
+
+
+            Console.WriteLine("----------------");
+
+            //Example 2
+
+            int[] myArray = { 1, 2, 3 };
+
+
+            try
+            {
+                Console.WriteLine(myArray[10]);
+            }
+            catch(IndexOutOfRangeException ex3)
+            {
+                Console.WriteLine($"Error: {ex3.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Execution Completed...");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
