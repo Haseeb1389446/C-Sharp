cac2280f6f27f645083234e4da955b4939865f3c
Haseeb1389446 <haseeb1389446@gmail.com>
2025-01-09 13:56:48 -0800

    Console application || practices

---
+++ Even_Odd_Checker/Even_Odd_Checker/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Odd_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a Number : ");
+
+            int number;
+            bool IsValid = int.TryParse(Console.ReadLine(), out number);
+
+            if (IsValid)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"{number} is Even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is Odd.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter a valid number.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
