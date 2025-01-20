82e215b055f6a4923d147cad03c63d56c0435c40
Haseeb1389446 <haseeb1389446@gmail.com>
2025-01-20 10:18:38 -0800

    Console application || practices

---
+++ Form_Calculater/Form_Calculater/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Form_Calculater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_num1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_num2 = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.btn_mul = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.lbl_rezult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(342, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculater Application";
+            // 
+            // txt_num1
+            // 
+            this.txt_num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_num1.Location = new System.Drawing.Point(606, 154);
+            this.txt_num1.Multiline = true;
+            this.txt_num1.Name = "txt_num1";
+            this.txt_num1.Size = new System.Drawing.Size(260, 32);
+            this.txt_num1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(264, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(302, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter First Number : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(264, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(300, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter Second Name : ";
+            // 
+            // txt_num2
+            // 
+            this.txt_num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_num2.Location = new System.Drawing.Point(606, 225);
+            this.txt_num2.Multiline = true;
+            this.txt_num2.Name = "txt_num2";
+            this.txt_num2.Size = new System.Drawing.Size(260, 32);
+            this.txt_num2.TabIndex = 3;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(271, 344);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(105, 33);
+            this.btn_add.TabIndex = 5;
+            this.btn_add.Text = "ADD";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sub.Location = new System.Drawing.Point(432, 344);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(105, 33);
+            this.btn_sub.TabIndex = 6;
+            this.btn_sub.Text = "SUB";
+            this.btn_sub.UseVisualStyleBackColor = false;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // btn_mul
+            // 
+            this.btn_mul.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_mul.Location = new System.Drawing.Point(601, 344);
+            this.btn_mul.Name = "btn_mul";
+            this.btn_mul.Size = new System.Drawing.Size(105, 33);
+            this.btn_mul.TabIndex = 7;
+            this.btn_mul.Text = "MUL";
+            this.btn_mul.UseVisualStyleBackColor = false;
+            this.btn_mul.Click += new System.EventHandler(this.btn_mul_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn_div.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_div.Location = new System.Drawing.Point(755, 344);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(105, 33);
+            this.btn_div.TabIndex = 8;
+            this.btn_div.Text = "DIV";
+            this.btn_div.UseVisualStyleBackColor = false;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // lbl_rezult
+            // 
+            this.lbl_rezult.AutoSize = true;
+            this.lbl_rezult.Font = new System.Drawing.Font("MV Boli", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.lbl_rezult.Location = new System.Drawing.Point(342, 471);
+            this.lbl_rezult.Name = "lbl_rezult";
+            this.lbl_rezult.Size = new System.Drawing.Size(0, 52);
+            this.lbl_rezult.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1130, 631);
+            this.Controls.Add(this.lbl_rezult);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_mul);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_num2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_num1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_num1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_num2;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button btn_mul;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.Label lbl_rezult;
+    }
+}
+
---
+++ Form_Calculater/Form_Calculater/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_Calculater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txt_num1.Text);
+            int num2 = int.Parse(txt_num2.Text);
+
+            int rezult = num1 + num2;
+
+            if (txt_num1.Text != "" && txt_num2.Text != "")
+            {
+                lbl_rezult.Text = "The Rezult Is " + rezult;
+            }
+            else
+            {
+                lbl_rezult.Text = "Plzzz Fill Out The Fields Properly...";
+            }
+
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txt_num1.Text);
+            int num2 = int.Parse(txt_num2.Text);
+
+            int rezult = num1 - num2;
+
+            if (txt_num1.Text != "" && txt_num2.Text != "")
+            {
+                lbl_rezult.Text = "The Rezult Is " + rezult;
+            }
+            else
+            {
+                lbl_rezult.Text = "Plzzz Fill Out The Fields Properly...";
+            }
+        }
+
+        private void btn_mul_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txt_num1.Text);
+            int num2 = int.Parse(txt_num2.Text);
+
+            int rezult = num1 * num2;
+
+            if (txt_num1.Text != "" && txt_num2.Text != "")
+            {
+                lbl_rezult.Text = "The Rezult Is " + rezult;
+            }
+            else
+            {
+                lbl_rezult.Text = "Plzzz Fill Out The Fields Properly...";
+            }
+
+
+
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(txt_num1.Text);
+            int num2 = int.Parse(txt_num2.Text);
+
+            int rezult = num1 / num2;
+
+            if (txt_num1.Text != "" && txt_num2.Text != "")
+            {
+                lbl_rezult.Text = "The Rezult Is " + rezult;
+            }
+            else
+            {
+                lbl_rezult.Text = "Plzzz Fill Out The Fields Properly...";
+            }
+        }
+    }
+}
